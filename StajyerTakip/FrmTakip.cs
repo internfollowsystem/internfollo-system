1d4a5089b25405bfef1ceeae867011d8d583ce9f
Sümeyye Özkaynak <s.ozkaynak@outlook.com>
2017-06-20 23:12:50 +0300

    Update FrmTakip.cs

--- StajyerTakip/FrmTakip.cs
+++ StajyerTakip/FrmTakip.cs
@@ -15,6 +15,30 @@ namespace StajyerTakip
         public FrmTakip()
         {
             InitializeComponent();
+            Connect cn= new Connect;
+            fr.Baglanti.Open();
+            dataGridView1.DataSource = cn.Listele();
+            //DataTable dt = baglanti.GetSchema("Tables");
+ 
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+                   // comboBox1.Items.Add(dt.Rows[i]["TABLE_NAME"]);
+                    //comboBox2.Items.Add(dt.Rows[i]["TABLE_NAME"]);
+                    //diye devam eder kaç taneyse artık
+            }
+            //baglanti.Close();
+        }
+        
+         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tablo = comboBox1.Text;
+            string sorgu="SELECT *FROM "+tablo;
+            da = new SqlDataAdapter(sorgu, baglanti);
+            ds = new DataSet();
+            baglanti.Open();
+            da.Fill(ds, tablo);
+            dataGridView1.DataSource = ds.Tables[tablo];
+            baglanti.Close();
         }
     }
 }
